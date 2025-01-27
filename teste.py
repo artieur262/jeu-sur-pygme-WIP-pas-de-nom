74c9811a9cc3fec686598fedb16852eba28ee903
artieur262 <130496437+artieur262@users.noreply.github.com>
2025-01-27 16:31:48 +0100

    vidage

--- teste.py
+++
@@ -1,9 +0,0 @@
-"est un test pour voir des evenement de pygame"
-from block.class_obj import pygame
-
-# pygame.mixer.init()
-# pyga
-
-while True:
-    for event in pygame.event.get():
-        print(event)
