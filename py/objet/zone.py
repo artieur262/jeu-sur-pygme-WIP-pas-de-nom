2a62c392164625b5782c2b70352b39f3fa475625
artieur262 <130496437+artieur262@users.noreply.github.com>
2025-04-29 10:42:57 +0200

    modif face et axe

--- py/objet/zone.py
+++ py/objet/zone.py
@@ -116,7 +116,8 @@ class Zone2D:
 class Zone3D(Zone2D):
     """class pour gérer les zones en 3D"""
 
-    LIST_FACE = ("x", "y", "z")
+    LIST_FACE = ("xy", "xz", "yz")
+    LIST_AXE = ("x", "y", "z")
 
     def __init__(self, coordonnee: list[int,int,int], taille: list[int,int,int]):
         super().__init__(coordonnee, taille)
