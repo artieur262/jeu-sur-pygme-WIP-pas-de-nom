0dc82aaef256ba75bb52c29fa804275f105583c3
artieur262 <130496437+artieur262@users.noreply.github.com>
2025-05-13 15:52:55 +0200

    ajoute activateur

---
+++ py/block/activateur.py
@@ -0,0 +1,33 @@
+from py.block.plateforme import Plateforme
+
+class Activateur(Plateforme):
+    """Activateur est une zone qui a pour but d'être affiché sur une surface
+    Args:
+        Zone (Zone): est la zone de l'objet graphique
+    """
+
+    def __init__(self, coordonnee: list[int], taille: tuple[int, int, int], couleur: tuple[int, int, int], sorti: int):
+        """initialise le bouton"""
+        super().__init__(coordonnee, taille, couleur)
+        self.__sorti = sorti
+        self.__activer = False
+    
+
+    def get_activer(self) -> bool:
+        """permet de savoir si le bloc logique est actif"""
+        return self.__activer
+
+    def set_activer(self, activer: bool) -> None:
+        """permet de changer l'etat du bloc logique"""
+        self.__activer = activer
+
+    def activation(self) -> None:
+        """permet d'activer le bloc logique"""
+        pass
+
+    def get_activation(self, output:set[int]) -> None:
+        """permet d'activer le bloc logique et ajouter les sorties dans le signal de output"""
+        if self.__activer:
+            output.add(self.__sorti)
+        
+            
