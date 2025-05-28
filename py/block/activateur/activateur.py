114272adf348cf88ad5eeb375b2e2d52a52b166d
artieur262 <130496437+artieur262@users.noreply.github.com>
2025-05-28 13:03:36 +0200

    ajout

--- py/block/activateur/activateur.py
+++ py/block/activateur/activateur.py
@@ -1,18 +1,18 @@
 from py.block.plateforme import Plateforme
 # from py.game.game import Map
 
-class Activateur(Plateforme):
+
+
+class Activateur:
     """Activateur est une zone qui a pour but d'être affiché sur une surface
     Args:
         Zone (Zone): est la zone de l'objet graphique
     """
 
-    def __init__(self, coordonnee: list[int], taille: tuple[int, int, int], couleur: tuple[int, int, int], sorti: int):
+    def __init__(self, sorti: int):
         """initialise le bouton"""
-        super().__init__(coordonnee, taille, couleur)
         self.__sorti = sorti
         self.__activer = False
-    
 
     def get_activer(self) -> bool:
         """permet de savoir si le bloc logique est actif"""
@@ -24,11 +24,28 @@ class Activateur(Plateforme):
 
     def activation(self, map) -> None:
         """permet d'activer le bloc logique"""
-        pass
+        raise NotImplementedError("la fonction n'est pas encore implémenté")
 
     def get_activation(self, output:set[int]) -> None:
         """permet d'activer le bloc logique et ajouter les sorties dans le signal de output"""
         if self.__activer:
             output.add(self.__sorti)
-        
-            
+    
+    def ajouter_map(self, map) -> None:
+        """ajoute la map"""
+        map.add_activateur(self)
+
+
+class ActivateurPlatforme(Plateforme, Activateur):
+    """Activateur est une zone qui a pour but d'être affiché sur une surface
+    Args:
+        Zone (Zone): est la zone de l'objet graphique
+    """
+    def __init__(self, coordonnee: list[int], taille: tuple[int, int, int], couleur: tuple[int, int, int], sorti: int):
+        """initialise le bouton"""
+        Plateforme.__init__(self, coordonnee, taille, couleur)
+        Activateur.__init__(self, sorti)
+
+    def ajouter_map(self, map):
+        Activateur.ajouter_map(self,map)
+        Plateforme.ajouter_map(self,map)
--- py/block/activateur/bouton.py
+++ py/block/activateur/bouton.py
@@ -1,19 +1,38 @@
-from py.block.activateur.activateur import Activateur
+from py.block.activateur.activateur import ActivateurPlatforme
 from py.objet.zone import Zone3D
 from py.interface.class_clavier import Clavier
 
-class Bouton(Activateur):
+
+class Bouton(ActivateurPlatforme):
     """Bouton est une zone qui a pour but d'être affiché sur une surface
     Args:
         Zone (Zone): est la zone de l'objet graphique
     """
 
-    def __init__(self, coordonnee: list[int], taille: tuple[int, int, int], couleur: tuple[int, int, int], sorti: int, zone_dectect: Zone3D):
+    def __init__(self, coordonnee: list[int], taille: tuple[int, int, int], couleur: tuple[int, int, int], sorti: int, rayon_dectect: int):
         """initialise le bouton"""
         super().__init__(coordonnee, taille, couleur, sorti)
-        self.__zone_dectect = zone_dectect
+        self.__rayon_dectect = rayon_dectect
 
+    
+    def get_zone_dectect(self, axe=-1) -> Zone3D:
+        """permet de savoir si le bloc logique est actif
+        Args:
+            axe (int, optional): axe de la zone de détection. Defaults to -1, ce qui signifie que la zone de détection est dans les 3 axes.
+                                la valeur de axe correspond à l'axe qui ne sera pas pris en compte pour la zone de détection.
+                                axe = 0 pour l'axe x, axe = 1 pour l'axe y, axe = 2 pour l'axe z.
+        
 
+        """
+        pos= []
+        taille = []
+        for i in range(3):
+            if axe == i :
+                pos.append(self.get_pos()[i])
+                taille.append(self.get_size()[i])
+            else:
+                pos.append(self.get_pos()[i] - self.__rayon_dectect)
+                taille.append(self.get_size()[i] + self.__rayon_dectect )
 
 class BoutonPush(Bouton):
     """Bouton_push est une zone qui a pour but d'être affiché sur une surface
@@ -21,9 +40,9 @@ class BoutonPush(Bouton):
         Zone (Zone): est la zone de l'objet graphique
     """
 
-    def __init__(self, coordonnee: list[int], taille: tuple[int, int, int], couleur: tuple[int, int, int], sorti: int, zone_dectect: Zone3D):
+    def __init__(self, coordonnee: list[int], taille: tuple[int, int, int], couleur: tuple[int, int, int], sorti: int, rayon_dectect: int):
         """initialise le bouton"""
-        super().__init__(coordonnee, taille, couleur, sorti, zone_dectect)
+        super().__init__(coordonnee, taille, couleur, sorti, rayon_dectect)
     
     def activation(self, map):
         """permet d'activer le bloc logique"""
@@ -31,7 +50,7 @@ class BoutonPush(Bouton):
         touche : dict[str, int] = map.get_game().get_touche()
         joueur: Zone3D = map.get_playeur()
 
-        if clavier.get_pression(touche["interaction"]) == "presser" and self.__zone_dectect.collision(joueur.get_pos(), joueur.get_size()):
+        if clavier.get_pression(touche["interaction"]) == "presser" and self.__rayon_dectect.collision(joueur.get_pos(), joueur.get_size()):
             self.set_activer(True)
         else:
             self.set_activer(False)
@@ -53,7 +72,7 @@ class BoutonSwitch(Bouton):
         touche : dict[str, int] = map.get_game().get_touche()
         joueur: Zone3D = map.get_playeur()
 
-        if clavier.get_pression(touche["interaction"]) == "vien_presser" and self.__zone_dectect.collision(joueur.get_pos(), joueur.get_size()):
+        if clavier.get_pression(touche["interaction"]) == "vien_presser" and self.__rayon_dectect.collision(joueur.get_pos(), joueur.get_size()):
             self.set_activer(not self.get_activer())
 
 
@@ -73,7 +92,8 @@ class BoutonImpulse(Bouton):
         touche : dict[str, int] = map.get_game().get_touche()
         joueur: Zone3D = map.get_playeur()
 
-        if clavier.get_pression(touche["interaction"]) == "vien_presser" and self.__zone_dectect.collision(joueur.get_pos(), joueur.get_size()):
+        if clavier.get_pression(touche["interaction"]) == "vien_presser" and self.__rayon_dectect.collision(joueur.get_pos(), joueur.get_size()):
             self.set_activer(True)
         else:
             self.set_activer(False)
+
