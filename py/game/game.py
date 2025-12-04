4e851a438db2f307c735c2f88d27956d2c8e1a06
artieur262 <raguinmateo@gmail.com>
2025-12-04 10:50:34 +0100

    correction d'import

--- py/game/game.py
+++ py/game/game.py
@@ -5,6 +5,8 @@ from py.graphique.graphique import screen
 from py.graphique.actualisation_pygame import actualise_event, change_fullscreen
 from py.graphique.image import Image
 from py.game.Map import Map
+from py.block.playeur import Playeur
+from py.objet.zone import Zone3D
 
 
 class Game:
