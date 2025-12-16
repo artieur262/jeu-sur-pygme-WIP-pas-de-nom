ce973998fdf228c83f0fbef9861d879a699e4d67
artieur262 <raguinmateo@gmail.com>
2025-12-16 10:36:02 +0100

    ajout de toute les types de diapo

--- py/block/activable/diapo.py
+++ py/block/activable/diapo.py
@@ -43,3 +43,70 @@ class Diapo(Activable, ObjetVisuel3D):
         """permet de mettre a jour l'image en fonction de l'etat"""
         self._set_image_list(self._texture_pack[self._index_texture])
 
+    def _actualise_activer(self, map_: "Map") -> None:
+        """permet de mettre a jour l'etat de l'activable"""
+        present_entre = map_.in_signal(self._entre)
+        self._activer = present_entre and not self.etat_precedent
+        self.etat_precedent = present_entre
+
+
+class DiapoBoucle(Diapo):
+    """cette class permet de faire un diapo en boucle
+    dés qu'elle arrive a la fin elle recommence au début
+    """
+
+    def activation(self, map_: "Map") -> None:
+        """permet d'activer le bloc logique"""
+        self._actualise_activer(map_)
+        if self.get_activer():
+            self._index_texture = (self._index_texture + 1) % len(self._texture_pack)
+            self._set_image_list(self._texture_pack[self._index_texture])
+            self.actualiser_image()
+
+
+class DiapoAllerRetour(Diapo):
+    """cette class permet de faire un diapo aller retour
+    dés qu'elle arrive a la fin elle va dans l'autre sens
+    """
+
+    def __init__(
+        self,
+        coordonnee: list[int],
+        taille: tuple[int, int, int],
+        texture_pack: list[tuple[Image, Image, Image]],
+        entre: int,
+    ):
+        """initialise le diapo aller retour"""
+        super().__init__(coordonnee, taille, texture_pack, entre)
+        self._direction = 1
+
+    def activation(self, map_: "Map") -> None:
+        """permet d'activer le bloc logique"""
+
+        self._actualise_activer(map_)
+
+        if self.get_activer():
+            self._index_texture += self._direction
+            if self._index_texture >= len(self._texture_pack):
+                self._index_texture = len(self._texture_pack) - 2
+                self._direction = -1
+            elif self._index_texture < 0:
+                self._index_texture = 1
+                self._direction = 1
+            self._set_image_list(self._texture_pack[self._index_texture])
+            self.actualiser_image()
+
+
+class DiapoUnique(Diapo):
+    """cette class permet de faire un diapo unique
+    dés qu'elle arrive a la fin elle reste a la dernière image
+    """
+
+    def activation(self, map_: "Map") -> None:
+        """permet d'activer le bloc logique"""
+        self._actualise_activer(map_)
+        if self.get_activer():
+            if self._index_texture < len(self._texture_pack) - 1:
+                self._index_texture += 1
+                self._set_image_list(self._texture_pack[self._index_texture])
+                self.actualiser_image()
