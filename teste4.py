329660ebb48a93b4329898bc94fdc37050fb8d41
artieur262 <raguinmateo@gmail.com>
2025-12-17 09:42:00 +0100

    corection en français car je ne suis pas espagnol

--- teste4.py
+++ teste4.py
@@ -1,27 +1,28 @@
 class Comparator:
     EGALE = "="
-    INFERIOR = "<"
-    SUPERIOR = ">"
-    INFERIOR_OU_IGUALE = "<="
-    SUPERIOR_OU_IGUALE = ">="
-    DIFERENTE = "!="
+    INFERIEUR = "<"
+    SUPERIEUR = ">"
+    INFERIEUR_OU_EGALE = "<="
+    SUPERIEUR_OU_EGALE = ">="
+    DIFERENT = "!="
 
     @staticmethod
     def compare(value1, value2, comparator: str) -> bool:
+        
         if comparator == Comparator.EGALE:
             return value1 == value2
-        elif comparator == Comparator.INFERIOR:
+        elif comparator == Comparator.INFERIEUR:
             return value1 < value2
-        elif comparator == Comparator.SUPERIOR:
+        elif comparator == Comparator.SUPERIEUR:
             return value1 > value2
-        elif comparator == Comparator.INFERIOR_OU_IGUALE:
+        elif comparator == Comparator.INFERIEUR_OU_EGALE:
             return value1 <= value2
-        elif comparator == Comparator.SUPERIOR_OU_IGUALE:
+        elif comparator == Comparator.SUPERIEUR_OU_EGALE:
             return value1 >= value2
-        elif comparator == Comparator.DIFERENTE:
+        elif comparator == Comparator.DIFERENT:
             return value1 != value2
         else:
-            raise ValueError(f"Comparador desconhecido: {comparator}")
+            raise ValueError(f"Comparateur inconnu: {comparator}")
 
 
 def main():
@@ -29,10 +30,9 @@ def main():
     b = Comparator.EGALE
     c = "B"
     print(Comparator.compare(a, "A", b))  # True
-    print(Comparator.compare(a, c, Comparator.DIFERENTE))  # True
+    print(Comparator.compare(a, c, Comparator.DIFERENT))  # True
     print(b)
     print(type(b))
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__m
