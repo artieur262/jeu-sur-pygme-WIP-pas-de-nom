c413cb92f8c8ad09d88567e52501989855d41bf7
artieur262 <raguinmateo@gmail.com>
2026-03-11 15:04:27 +0100

    modification de element_it

--- py/interface/element_it.py
+++ py/interface/element_it.py
@@ -1,4 +1,11 @@
-class ElementInterface:
+from typing import TYPE_CHECKING
+from py.objet.zone import Zone2D
+
+if TYPE_CHECKING:
+    import pygame
+
+
+class ElementInterface(Zone2D):
     """Interface pour les éléments interactifs.
 
     args:
@@ -6,18 +13,21 @@ class ElementInterface:
         parent (ElementInterface, optional): est l'interface parente. Defaults to None.
     """
 
-    def __init__(self, taille_auto: bool, parent: "ElementInterface" = None) -> None:
+    def __init__(
+        self,
+        position: tuple[int, int],
+        size: tuple[int, int],
+        taille_auto: bool,
+        parent: "ElementInterface" = None,
+    ) -> None:
         """initialise l'interface
 
         Args:
             parent (ElementInterface, optional): est l'interface parente. Defaults to None.
         """
+        Zone2D.__init__(self, position, size)
         self._taille_auto = taille_auto
         self._parent = parent
-
-    def get_taille_auto(self) -> bool:
-        """get la taille auto de l'interface"""
-        return self._taille_auto
 
     def get_parent(self) -> "ElementInterface":
         """get l'interface parente de l'interface"""
@@ -26,3 +36,21 @@ class ElementInterface:
     def set_parent(self, parent: "ElementInterface") -> None:
         """set l'interface parente de l'interface"""
         self._parent = parent
+
+    def actualise_taille(self) -> None:
+        """actualise la taille de la box en fonction de ses ellement"""
+        raise NotImplementedError(
+            "Cette méthode doit être implémentée par les sous-classes."
+        )
+
+    def affiche(
+        self, decalage: tuple[int, int] = None, surface: "pygame.Surface" | None = None
+    ) -> None:
+        """affiche l'interface sur la surface donnée
+
+        Args:
+            surface (pygame.Surface): est la surface sur laquelle afficher l'interface
+        """
+        raise NotImplementedError(
+            "Cette méthode doit être implémentée par les sous-classes."
+        )
